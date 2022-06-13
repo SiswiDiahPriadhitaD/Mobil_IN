7bb3811640af9c8fd645b8c1af94f41b8de1a1ff
Your Name <you@example.com>
2022-06-13 12:14:31 +0700

    Iftitah | Fix Controller

--- resources/views/customer/elements/navbar.blade.php
+++ resources/views/customer/elements/navbar.blade.php
@@ -7,31 +7,59 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item me-4">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item me-4">
-          <a class="nav-link" aria-current="page" href="#">Search</a>
+          <!-- Button trigger modal -->
+          <a class="nav-link" aria-current="page" data-bs-toggle="modal" data-bs-target="#exampleModal" href="#">Search</a>
         </li>
         <li class="nav-item me-4 dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Brands
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            @foreach (\App\Http\Controllers\Customer\HomeController::brand() as $item)                
+              <li><a class="dropdown-item" href="/product/{{ $item->id }}/brand">{{ $item->brand }}</a></li>
+            @endforeach
           </ul>
         </li>
+        @auth
+          <li class="nav-item me-4">
+            <!-- Button trigger modal -->
+            <a class="nav-link" aria-current="page" href="/transaction">Transactions</a>
+          </li>
+        @endauth
         {{-- <li class="nav-item me-4">
           <a class="nav-link" aria-current="page" href="#">About</a>
         </li> --}}
-        <li class="nav-item me-1">
-          <a class="btn btn-dark btn-md px-5" href="/login">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="btn btn-dark btn-md px-5" href="/register">Register</a>
-        </li>
+        @auth
+          <li class="nav-item me-1">
+            @if (Auth::user()->role == 'admin')                
+              <a class="btn btn-warning text-white btn-md px-5" href="/admin/home">Admin</a>
+            @else
+              <a class="btn btn-warning text-white btn-md px-5" href="/profile">
+                Profile</a>
+            @endif
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-warning text-white btn-md px-5" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+          </li>
+        @else
+          <li class="nav-item me-1">
+            <a class="btn btn-dark btn-md px-5" href="/login">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-dark btn-md px-5" href="/register">Register</a>
+          </li>
+        @endauth
       </ul>
       
     </div>
